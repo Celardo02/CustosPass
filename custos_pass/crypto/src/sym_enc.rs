204144bfc1de3a665a0b31a717a714d98f961eac
Celardo02 <matteo.celardo.social@gmail.com>
2025-09-17 19:09:41 +0200

    Crypto: trait method 'encryption' temporary implementation
    Temporary implementation for the encryption method is terminated.
    Major changes not implemented yet:
    - aes_key is obtained through the argument key itself; a key derivation
      function algorithm output must be used instead
    - SymEncProvider srtucture only stores old nonces values, while it
      should store a Vec of nonces for each key that has already been used
      (the will be stored as its hash)

--- custos_pass/crypto/src/sym_enc.rs
+++ custos_pass/crypto/src/sym_enc.rs
@@ -11,46 +11,126 @@
 //! Nonce reuse is prevented by the module implementation. 
 
 
-pub  mod sym_enc {
+use aws_lc_rs::aead::{self, Aad, RandomizedNonceKey, Nonce, AES_256_GCM};
+use crate::crypto_errors::{CryptoErr, CryptoErrOrigin, CryptoErrKind};
+use secure_string::{SecureArray, SecureVec};
 
-    use aws_lc_rs::aead::{self, Aad, RandomizedNonceKey, Nonce, AES_256_GCM};
-    use std::{convert, io};
+// Aes algorithm variant that will be used 
+const AES_ALG: &aead::Algorithm = &AES_256_GCM;
+// lenght of the aes key in bytes
+pub const KEY_LEN: usize = 32;
 
-    // Aes algorithm variant that will be used 
-    const AES_ALG: &aead::Algorithm = &AES_256_GCM;
-    // lenght of the aes key in bytes
-    const KEY_LEN: usize = 32;
+// SymEncResult struct [[[
 
-    pub trait SymmetricEnc {
-        fn encrypt<T> (&self, key: [u8; KEY_LEN], aad: Aad<T>, plain: Vec<u8> ) -> Result<Vec<u8>, io::Error> 
-            where
-                T: convert::AsRef<[u8]>;
-        fn decrypt<T> (&self, key: [u8; KEY_LEN], aad: Aad<T>, enc: Vec<u8>) -> Result<Vec<u8>, io::Error>
-            where
-                T: convert::AsRef<[u8]>;
-        fn get_old_nonces(&self) -> Vec<Nonce>;
+pub struct SymEncResult {
+    output: SecureVec<u8>,
+    nonce: Nonce
+}
+
+impl SymEncResult {
+    pub fn new(output: SecureVec<u8>, nonce: Nonce) -> SymEncResult {
+        SymEncResult {
+            output,
+            nonce
+        }
     }
 
-    pub struct SymEncProvider {
-        old_nonces: Vec<Nonce>
+    pub fn get_output (&self) -> &SecureVec<u8> {
+        &self.output
     }
 
-    impl SymmetricEnc for SymEncProvider {
-
-        fn encrypt<T> (&self, key: [u8; KEY_LEN], aad: Aad<T>, plain: Vec<u8> ) -> Result<Vec<u8>, io::Error> 
-            where
-                T: convert::AsRef<[u8]>{
-            unimplemented!();
-        }
-
-        fn decrypt<T> (&self, key: [u8; KEY_LEN], aad: Aad<T>, enc: Vec<u8>) -> Result<Vec<u8>, io::Error>
-            where
-                T: convert::AsRef<[u8]> {
-            unimplemented!();
-        }
-        fn get_old_nonces(&self) -> Vec<Nonce> {
-            unimplemented!();
-        }
-
+    pub fn get_nonce (&self) -> &Nonce {
+        &self.nonce
     }
 }
+
+// ]]]
+
+// SymEncProvider [[[
+
+pub struct SymEncProvider{
+    // IMPORTANT: this struct must be changed with an hash map -> 
+    //      key = hash of the key used in the encryption 
+    //      value = associated nonce
+    // all the struct methods must be changed accordingly
+    old_nonces: Vec<Nonce>
+}
+
+impl SymEncProvider {
+    pub fn new(old_nonces: Vec<Nonce>) -> SymEncProvider {
+        SymEncProvider {
+            old_nonces
+        }
+    }
+
+    pub fn new_empty() -> SymEncProvider {
+        SymEncProvider {
+            old_nonces: Vec::new()
+        }
+    }
+
+
+    fn contains_nonce(&self, nonce: &Nonce) -> bool {
+        self.old_nonces.iter().any(|n| n.as_ref() == nonce.as_ref())
+    }
+
+    fn try_encrypt(&self, aes_key: &RandomizedNonceKey, plain: &SecureVec<u8>, aad: Aad<Vec<u8>>) -> Result<SymEncResult, CryptoErr> {
+        let mut out = Vec::from(plain.unsecure());
+
+        let nonce = aes_key.seal_in_place_append_tag(aad, &mut out)
+            .map_err(|_| CryptoErr::new(CryptoErrKind::EncryptionFailed, CryptoErrOrigin::SymmetricEncryption, "aes encryption process failed"))?;
+
+        Ok(SymEncResult::new(SecureVec::from(out), nonce))
+    }
+
+}
+
+// ]]]
+
+// SymmetricEnc trait [[[
+
+pub trait SymmetricEnc {
+    fn encrypt (&mut self, key: &SecureArray<u8, KEY_LEN>, aad: Aad<Vec<u8>>, plain: &SecureVec<u8> ) -> Result<SymEncResult, CryptoErr>;
+    fn decrypt (&self, key: &SecureArray<u8, KEY_LEN>, aad: Aad<Vec<u8>>, enc: &SecureVec<u8>) -> Result<SecureVec<u8>, CryptoErr>;
+    fn get_old_nonces(&self) -> &Vec<Nonce>;
+}
+
+impl SymmetricEnc for SymEncProvider {
+
+    fn encrypt (&mut self, key: &SecureArray<u8, KEY_LEN>, aad: Aad<Vec<u8>>, plain: &SecureVec<u8> ) -> Result<SymEncResult, CryptoErr> {
+
+        // checking whether plain text is empty or not
+        if plain.unsecure().is_empty() { 
+            return Err(CryptoErr::new(CryptoErrKind::InpuNullOrEmpty, CryptoErrOrigin::SymmetricEncryption, "plain text is empty"));
+        }
+
+        // IMPORTANT: kdf function output must be used instead of key.unsecure()
+        let aes_key = RandomizedNonceKey::new(AES_ALG, key.unsecure())
+            .map_err(|_| CryptoErr::new(CryptoErrKind::AesKeyGenFailed, CryptoErrOrigin::SymmetricEncryption, "aes key generation process failed"))?;
+
+        let mut res = self.try_encrypt(&aes_key, &plain, Aad::from(Vec::from(aad.as_ref())))?;
+
+        // if the nonce has already been used, the encryption process is repeated.
+        // Even though this is really inefficient in theory, the probability that a specific 
+        // nonce has already been used for a specificy derived key value is almost impossible, 
+        // hence this implementation compromise between code readability and efficiency
+        while self.contains_nonce(&res.get_nonce()) {
+            res = self.try_encrypt(&aes_key, &plain, Aad::from(Vec::from(aad.as_ref())))?;
+        }
+            
+        self.old_nonces.push(Nonce::from(res.get_nonce().as_ref()));
+
+        Ok(res)
+    }
+
+    fn decrypt (&self, key: &SecureArray<u8, KEY_LEN>, aad: Aad<Vec<u8>>, enc: &SecureVec<u8>) -> Result<SecureVec<u8>, CryptoErr>{
+        unimplemented!();
+    }
+
+    fn get_old_nonces (&self) -> &Vec<Nonce> {
+        &self.old_nonces
+    }
+
+}
+
+// ]]]
