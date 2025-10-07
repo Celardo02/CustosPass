a1a4a361513b6fad45f1e2a2c422adda0b303e2f
Celardo02 <matteo.celardo.social@gmail.com>
2025-10-07 19:43:33 +0200

    Domain: added more tests to mst_pwd
    All the trait methods are now tested

--- custos_pass/domain/src/mst_pwd.rs
+++ custos_pass/domain/src/mst_pwd.rs
@@ -98,26 +98,92 @@ impl MstPwd for Master {
 
 mod test {
     use super::*;
-    use crypto::{SecureBytes, CryptoProvider, hashing::{Hashing, SHA512_OUTPUT_LEN}, rng::SystemRandom,};
+    use crypto::{SecureBytes, hashing::SALT_LEN};
 
     // seconds in a day
     const DAY: i64 = 60 * 60 * 24;
 
+    // new [[[
+    /// Tests that `new` returns a `Master` instance that expires after 3 months
+    #[test]
+    fn new_3_months () {
+        let hv = HashVal::new(SecureBytes::new(Vec::from("master")), [1u8; SALT_LEN])
+            .expect("unable to create HashVal");
+        let m = Master::new(hv);
+
+        assert_eq!(
+            m.exp_date,
+            Utc::now().date_naive().checked_add_signed(TimeDelta::seconds(MST_EXP)).unwrap(),
+            "new associated function does not set expiration date properly"
+        );
+    }
+
+    /// Tests that `new` actually sets `hash_val` field with `hash` argument
+    #[test]
+    fn new_value(){
+        let hv = HashVal::new(SecureBytes::new(Vec::from("master")), [1u8; SALT_LEN])
+            .expect("unable to create HashVal");
+        let m = Master::new(hv.clone());
+
+        assert_eq!(hv, m.hash_val, "hash argument is ignored");
+    }
+    // ]]]
+
+    // new with date [[[
+    /// Tests that `new_with_date` actually sets `hash_val` and `exp_date` fields with the provided
+    /// arguments
+    #[test]
+    fn new_with_date_value(){
+        let hv = HashVal::new(SecureBytes::new(Vec::from("master")), [1u8; SALT_LEN])
+            .expect("unable to create HashVal");
+        let exp = Utc::now().date_naive().checked_add_signed(TimeDelta::seconds(DAY)).unwrap();
+        let m = Master::new_with_date(hv.clone(), exp.clone())
+            .expect("unable to create Master");
+
+        assert_eq!(hv, m.hash_val, "hash argument is ignored");
+        assert_eq!(exp, m.exp_date, "exp_date argument is ignored");
+    }
+
+    /// Tests that `new_with_date` returns an error if `exp_date` argument predates 
+    /// the current day
+    #[test]
+    fn new_with_date_predate(){
+        let hv = HashVal::new(SecureBytes::new(Vec::from("master")), [1u8; SALT_LEN])
+            .expect("unable to create HashVal");
+        let exp = Utc::now().date_naive().checked_add_signed(TimeDelta::seconds(-DAY)).unwrap();
+
+        assert!(
+            Master::new_with_date(hv, exp).is_err(),
+            "no error when exp_date predates the current day"
+        );
+    }
+
+    /// Tests that `new_with_date` returns an error if `exp_date` argument is equal to 
+    /// the current day
+    #[test]
+    fn new_with_date_current_date(){
+        let hv = HashVal::new(SecureBytes::new(Vec::from("master")), [1u8; SALT_LEN])
+            .expect("unable to create HashVal");
+        let exp = Utc::now().date_naive();
+
+        assert!(
+            Master::new_with_date(hv, exp).is_err(),
+            "no error when exp_date is equal to the current day"
+        );
+    }
+    // ]]]
+
+    // set hash val [[[
+
     /// Tests that `set_hash_val` updates the expiration date
     #[test]
-    fn set_hash_val_update() {
-        let pwd = SecureBytes::new(Vec::from("pwd"));
-        let mut cp = CryptoProvider::new_empty(SystemRandom::new())
-            .expect("unable to create CryptoProvider");
+    fn set_hash_val_date() {
+        let hv = HashVal::new(SecureBytes::new(Vec::from("master")), [1u8; SALT_LEN])
+            .expect("unable to create HashVal");
 
-        let hv = cp.derive_hash(&pwd, SHA512_OUTPUT_LEN)
-                .expect("unable to derive HashVal");
+        let exp = Utc::now().date_naive().checked_add_signed(TimeDelta::seconds(DAY)).unwrap();
 
-        let exp = Utc::now().date_naive()
-            .checked_add_signed(TimeDelta::seconds(DAY))
-            .expect("unable to get current date + 1 day");
-
-        let mut m = Master::new(hv.clone(), exp)
+        let mut m = Master::new_with_date(hv.clone(), exp)
             .expect("unable to create m");
 
         m.set_hash_val(hv);
@@ -127,11 +193,54 @@ mod test {
             &Utc::now()
                 .date_naive()
                 .checked_add_signed(TimeDelta::seconds(MST_EXP))
-                .expect("unable to get current date + MST_EXP"),
+                .unwrap(),
             "new expiration date has not got a time delta of MST_EXP"
         );
-
     }
+
+    /// Tests that `set_hash_val` updates `hash_val` field value
+    #[test]
+    fn set_hash_val_update() {
+        let hv = HashVal::new(SecureBytes::new(Vec::from("master")), [1u8; SALT_LEN])
+            .expect("unable to create HashVal");
+
+        let exp = Utc::now().date_naive().checked_add_signed(TimeDelta::seconds(DAY)).unwrap();
+
+        let mut m = Master::new_with_date(hv, exp)
+            .expect("unable to create m");
+
+        let hv2 = HashVal::new(SecureBytes::new(Vec::from("new_master")), [1u8; SALT_LEN])
+            .expect("unable to create HashVal 2");
+
+        m.set_hash_val(hv2.clone());
+
+        assert_eq!(m.get_hash_val(), &hv2, "set_hash_val does not update hash val field");
+    }
+
+    // ]]]
+
+    /// Tests that `get_hash_val` actually returns `hash_val` field value
+    #[test]
+    fn get_hash_val_value() {
+        let hv = HashVal::new(SecureBytes::new(Vec::from("master")), [1u8; SALT_LEN])
+            .expect("unable to create HashVal");
+        let m = Master::new(hv.clone());
+
+        assert_eq!(&hv, m.get_hash_val(), "get_hash_val does not return the correct value");
+    }
+
+    /// Tests that `get_exp_date` actually returns `exp_date` field value
+    #[test]
+    fn get_exp_date_value() {
+        let hv = HashVal::new(SecureBytes::new(Vec::from("master")), [1u8; SALT_LEN])
+            .expect("unable to create HashVal");
+        let exp = Utc::now().date_naive().checked_add_signed(TimeDelta::seconds(DAY)).unwrap();
+        let m = Master::new_with_date(hv, exp.clone())
+            .expect("unable to create Master");
+
+        assert_eq!(&exp, m.get_exp_date(), "get_exp_date does not return the correct value");
+    }
+
 }
 
 // ]]
