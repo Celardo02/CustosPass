320dd9d8ddfe6e47c4ba3e26ad53591aca68e374
Celardo02 <matteo.celardo.social@gmail.com>
2025-10-27 11:08:05 +0100

    Storage: added self to PersCredSet and implemented delete
    - &self or &mut self has been added to all the methods in PersCredSet
      trait, hence also in CredStorage implementation of the trait
    - delete method of CredStorage was implemented
    - documentation comment fixes in PersCredSet
    - added a comment at the beginning of update in CredStorage

--- custos_pass/storage/src/pers_cred.rs
+++ custos_pass/storage/src/pers_cred.rs
@@ -21,8 +21,8 @@ pub trait PersCredSet<C: CredSet> {
     ///
     /// # Returns
     ///
-    /// Returns `()` the id of `cred_set` is unique, `Err` otherwise
-    fn create(cred_set: C) -> Result<(), Err>;
+    /// Returns `()` if the id of `cred_set` is unique, `Err` otherwise
+    fn create(&mut self, cred_set: C) -> Result<(), Err>;
 
     /// Searches a credential set with the given id.
     ///
@@ -33,9 +33,10 @@ pub trait PersCredSet<C: CredSet> {
     /// # Returns
     ///
     /// Returns the searched credential set if exists; `None` otherwise.
-    fn read_id(id: &String) -> Option<&C>;
+    fn read_id(&self, id: &String) -> Option<&C>;
 
-    /// Searches all the credential sets that contain the given string within their id or username.
+    /// Searches all the credential sets that contain the given string within their id, username 
+    /// or notes fields.
     ///
     /// # Parameters 
     ///
@@ -43,9 +44,9 @@ pub trait PersCredSet<C: CredSet> {
     ///
     /// # Returns
     ///
-    /// Returns a `Vec` of credential sets that contain `text` in their id or username if they
-    /// exist; `None` otherwise.
-    fn read_text(text: &String) -> Option<Vec<&C>>;
+    /// Returns a `Vec` of credential sets that contain `text` in their id, username, or notes 
+    /// if they exist; `None` otherwise.
+    fn read_text(&self, text: &String) -> Option<Vec<&C>>;
 
     /// Updates a credential set with the given id.
     ///
@@ -57,8 +58,8 @@ pub trait PersCredSet<C: CredSet> {
     /// # Returns 
     ///
     /// Returns `()` if a credential set with the given id exists and the id in `cred_set` is
-    /// unique, `Err` otherwise.
-    fn update(id: &String, cred_set: C) -> Result<(), Err>;
+    /// unique or equal to `id`, `Err` otherwise.
+    fn update(&mut self, id: &String, cred_set: C) -> Result<(), Err>;
 
     /// Deletes a credential set with the given id.
     ///
@@ -69,16 +70,16 @@ pub trait PersCredSet<C: CredSet> {
     /// # Returns
     ///
     /// Returns `()` if a credential set with the given id exists, `Err` otherwise.
-    fn delete(id: &String) -> Result<(), Err>;
+    fn delete(&mut self, id: &String) -> Result<(), Err>;
 
     /// Returns all the credential sets in the storage layer if they exist.
-    fn list_all() -> Option<Vec<C>>;
+    fn list_all(&self) -> Option<Vec<C>>;
 
     /// Returns all the expired credential sets if they exist.
-    fn check_expired() -> Option<Vec<C>>;
+    fn check_expired(&self) -> Option<Vec<C>>;
 
     /// Returns all the non-expiring credential sets if they exist.
-    fn check_non_expiring() -> Option<Vec<C>>;
+    fn check_non_expiring(&self) -> Option<Vec<C>>;
 }
 
 // ]]]
@@ -98,7 +99,7 @@ pub struct CredStorage <C: CredSet> {
     /// must be ensured
     cs_vec: Vec<C>,
     /// Avl tree containing a copy of all credential sets keys associated with their index in 
-    /// `cs_vec`. This field is used to optimize delete and update operations.
+    /// `cs_vec`. This field is used to optimize search by complete id, delete and update operations.
     cs_tree: AvlTreeMap<String, usize>
 }
 
@@ -138,37 +139,108 @@ impl <C: CredSet> CredStorage<C> {
 // PersCredSet for CredStorage [[[
 
 impl <C: CredSet> PersCredSet<C> for CredStorage<C> {
-    fn create(cred_set: C) -> Result<(), Err>{
+    fn create(&mut self, cred_set: C) -> Result<(), Err>{
+        if self.cs_tree.contains_key(cred_set.get_id()) {
+            return Err(Err::new("Credential set id is already in use", ErrSrc::Storage))
+        }
+
+        // the index of cred_set in cs_vec will be the current length of cs_vec as cred_set will 
+        // be appended to it
+        self.cs_tree.insert(cred_set.get_id().clone(), self.cs_vec.len());
+        self.cs_vec.push(cred_set);
+
+        Ok(())
+
+    }
+
+    fn read_id(&self, id: &String) -> Option<&C>{
+        let mut cred = None;
+
+        if let Some(index) = self.cs_tree.get(id) {
+            cred = self.cs_vec.get(*index);
+        }
+
+        cred
+    }
+
+
+    fn read_text(&self, text: &String) -> Option<Vec<&C>>{
         unimplemented!();
     }
 
-    fn read_id(id: &String) -> Option<&C>{
+
+    fn update(&mut self, id: &String, cred_set: C) -> Result<(), Err>{
+        // Returns an error if cred_set.get_id has been updated (different from the old one, 
+        // which is id) and is already in use
+        if id != cred_set.get_id() && self.cs_tree.contains_key(cred_set.get_id()) {
+            return Err(Err::new("Updated credential set id is already in use", ErrSrc::Storage))
+        }
+
+        if id == cred_set.get_id() {
+            // no cs_tree update needed as the updated id in cred_set is equal to the old one
+            let c = self.cs_vec.get_mut(
+                *self.cs_tree
+                    .get(id)
+                    .expect("Unable to get an existing credential set index")
+            ).expect("unable to get a mutable reference to an existing credential set");
+
+            *c = cred_set;
+        } else {
+            // cs_tree update needed as the updated id in cred_set is not equal to the one. 
+            // Therefore, same cs_vec index must be associated to the new id value
+            let index = self.cs_tree
+                .remove(id)
+                .expect("Unable to remove an existing credential set index");
+
+            let c = self.cs_vec
+                .get_mut(index)
+                .expect("Unable to get a mutable reference to an existing credential set");
+
+            self.cs_tree.insert(cred_set.get_id().clone(), index);
+
+            *c = cred_set;
+        }
+
+        Ok(())
+    }
+
+    fn delete(&mut self, id: &String) -> Result<(), Err>{
+        if !self.cs_tree.contains_key(id) {
+            return Err(Err::new("Credential set id to delete does not exist", ErrSrc::Storage))
+        }
+        
+        let index = self.cs_tree
+            .remove(id)
+            .expect("Unable to remove an existing credential set index");
+
+        self.cs_vec.swap_remove(index);
+
+        // index updates must take place only if the deleted element was not the only one in 
+        // cs_tree
+        if !self.cs_tree.is_empty() {
+            // updating the index of the element in cs_vec that was swapped with the deleted one
+            let swapped = self.cs_tree.get_mut(
+                self.cs_vec
+                    .get(index)
+                    .expect("Unable to get the swapped credential set value")
+                    .get_id()
+            ).expect("Unable to get a mutable reference to an existing credential set index");
+
+            *swapped = index;
+        }
+
+        Ok(())
+    }
+
+    fn list_all(&self) -> Option<Vec<C>>{
         unimplemented!();
     }
 
-
-    fn read_text(text: &String) -> Option<Vec<&C>>{
+    fn check_expired(&self) -> Option<Vec<C>>{
         unimplemented!();
     }
 
-
-    fn update(id: &String, cred_set: C) -> Result<(), Err>{
-        unimplemented!();
-    }
-
-    fn delete(id: &String) -> Result<(), Err>{
-        unimplemented!();
-    }
-
-    fn list_all() -> Option<Vec<C>>{
-        unimplemented!();
-    }
-
-    fn check_expired() -> Option<Vec<C>>{
-        unimplemented!();
-    }
-
-    fn check_non_expiring() -> Option<Vec<C>>{
+    fn check_non_expiring(&self) -> Option<Vec<C>>{
         unimplemented!();
     }
 
